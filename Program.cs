6357835d83fa12d663990e0992a687cff3e1ffd3
Serg Ch <chivirdos@gmail.com>
2019-10-04 22:18:16 +0300

    Считывание файлов в список, объединение списков и сортировка

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ namespace CSIO
 {
     class Program
     {
+        static string first = "first.txt";
+        static string second = "second.txt";
         static void Main(string[] args)
         {
-            var first = "first.txt";
-            var second = "second.txt";
-            System.Console.Write("Enter length of files: ");
+            
+            System.Console.Write("Введите длину файлов: ");
             var lengthOfFile = int.Parse(Console.ReadLine());
 
             #region Создание файлов
-            createFiles(first, lengthOfFile, 1, 50);
-            createFiles(second, lengthOfFile, 50, 100);
+            createFiles(first, lengthOfFile);
+            createFiles(second, lengthOfFile);
             System.Console.WriteLine("Записано!");
             #endregion
 
@@ -25,6 +26,11 @@ namespace CSIO
             sortFiles(second);
             System.Console.WriteLine("Отсортировано!");
             #endregion
+
+            createResult();
+            System.Console.WriteLine("Готово!");
+
+
         }
 
         static void sortFiles(string nameOfFile)
@@ -51,16 +57,70 @@ namespace CSIO
 
         }
 
-        static void createFiles(string nameOfFile, int lengthOfFile, int min, int max)
+        static void createFiles(string nameOfFile, int lengthOfFile)
         {
             using (var sw = new StreamWriter(nameOfFile, false, Encoding.UTF8))
             {
                 var rand = new Random();
                 for(int i = 0; i < lengthOfFile; i++)
                 {
-                    sw.WriteLine(rand.Next(min, max));
+                    sw.WriteLine(rand.Next(1, 100));
                 }
             }
+        }
+
+        static void createResult()
+        {
+            var resultFile = "result.txt";
+            var firstList = grabFileToList(first);
+            var secondList = grabFileToList(second);
+
+            var resultList = new List<int>();
+            resultList.AddRange(firstList);
+            resultList.AddRange(secondList);
+
+            bubbleSort(resultList);
+
+            using (var sw = new StreamWriter(resultFile, false, Encoding.UTF8))
+            {
+                for(var i = 0; i < resultList.Count; i++)
+                {
+                    sw.WriteLine(resultList[i]);
+                }
+            }
+
+        }
+
+        static List<int> grabFileToList(string fileName)
+        {
+            var numList = new List<int>();
+            using (var sr = new StreamReader(fileName, Encoding.UTF8))
+            {
+                while(!sr.EndOfStream)
+                {
+                    numList.Add(int.Parse(sr.ReadLine()));
+                }
+            }
+
+            return numList;
+        }
+
+        static List<int> bubbleSort(List<int> list)
+        {
+            int temp;
+            for(int i = 0; i < list.Count; i++)
+            {
+                for(int j = i + 1; j < list.Count; j++)
+                {
+                    if(list[i] > list[j])
+                    {
+                        temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+            return list;
         }
     }
 }
